d493c84ba4a9d127907ccb766f7b4f8f19a52a14
yasutakatou <katou.pfu@jp.fujitsu.com>
2019-10-07 13:08:52 +0900

    change

--- main.go
+++ main.go
@@ -16,82 +16,205 @@ limitations under the License.
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os/exec"
-	"strings"
-
-	"github.com/kr/pty"
-	"golang.org/x/net/websocket"
+  "flag"
+  "fmt"
+  "github.com/kr/pty"
+  "golang.org/x/net/websocket"
+  "io"
+  "log"
+  "net/http"
+  "os"
+  "os/exec"
+  "strings"
+  "time"
+  "bytes"
 )
 
-const (
-	webAssetsDir = "www"
-	listenAddr   = "0.0.0.0:5000"
-)
+var tty *os.File
+var debug bool = false
+var noauth bool = false
+var HEARTBEATCOUNT int = 30
+var HEARTBEAT int = 10
+var liveCount int
+const patternLen = 5
+var port string = "32000"
+var webAssetsDir string
+var passwd string
 
 type Handler struct {
-	fileServer http.Handler
+  fileServer http.Handler
+}
+
+type InputWrapper struct {
+  ws *websocket.Conn
+}
+
+// ignoredInputs are the strange input bytes that we look for and drop
+var ignoredInputs = [][patternLen]byte{
+  {27, 91, 62, 48, 59},
+  {27, 80, 48, 43, 114},
 }
 
 func main() {
-	fmt.Printf("Listening on http://%s\n", listenAddr)
-	http.ListenAndServe(listenAddr, &Handler{
-		fileServer: http.FileServer(http.Dir(webAssetsDir)),
-	})
+  _passwd       := flag.String("passwd","passwd","[-token=ACCESS PASSWORD]")
+  _webAssetsDir := flag.String("html","/var/www/html","[-html=HTML DIR]")
+  _command      := flag.String("command","/bin/bash","[-command=EXECUTE COMMAND]")
+  _port         := flag.String("port","32000","[-port=LISTENING PORT]")
+  _debug        := flag.Bool("debug",false,"[-debug=DEBUG MODE]")
+  _noauth       := flag.Bool("noauth",false,"[-noauth=DON'T USE BASIC AUTH]")
+  _timeout      := flag.Int("timeout",30,"[-timeout=CONNECTION TIMEOUT]")
+
+  flag.Parse()
+
+  passwd             = string(*_passwd)
+  webAssetsDir       = string(*_webAssetsDir)
+  command           := string(*_command)
+  port               = string(*_port)
+  debug              = bool(*_debug)
+  noauth             = bool(*_noauth)
+  HEARTBEATCOUNT     = int(*_timeout)
+
+  if debug == true {
+    fmt.Println(" ---------")
+    fmt.Println(" |passwd | " + passwd)
+    fmt.Println(" |html   | " + webAssetsDir)
+    fmt.Println(" |command| " + command)
+    fmt.Println(" |port   | " + port)
+     fmt.Printf(" |debug  | %t\n",debug)
+     fmt.Printf(" |noauth | %t\n",noauth)
+     fmt.Printf(" |timeout| %d\n",HEARTBEATCOUNT)
+    fmt.Println(" ---------")
+  }
+
+  // - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = - = -
+
+  cmd := exec.Command(os.Getenv("SHELL"), "-c",command)
+  tty, _ = pty.Start(cmd)
+  defer tty.Close()
+
+  fmt.Printf("Server listening on port %s.\n", port)
+  http.ListenAndServe("0.0.0.0:"+port, &Handler{
+    fileServer: http.FileServer(http.Dir(webAssetsDir)),
+  })
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%v %v", r.Method, r.URL.Path)
-	// need to serve shell via websocket?
-	if strings.Trim(r.URL.Path, "/") == "shell" {
-		onShell(w, r)
-		return
-	}
-	// serve static assets from 'static' dir:
-	h.fileServer.ServeHTTP(w, r)
+  if debug == true { 
+    log.Printf("%v %v", r.Method, r.URL.Path)
+  }
+
+  if noauth == false {
+    token := r.URL.Query().Get("token")
+
+    if liveCount > 0 {
+      fmt.Fprintf(w, "%s connection exists. timeout count: %d\n", r.RemoteAddr,liveCount)
+      return
+    }
+
+    if strings.Trim(r.URL.Path, "/") == "" {
+      if token != passwd {
+        if debug == true { 
+          fmt.Printf("token auth error: %s\n", token)
+        }
+        fmt.Fprintf(w, "token auth error. ")
+        return
+      }
+    }
+  }
+
+  // need to serve shell via websocket?
+  if strings.Trim(r.URL.Path, "/") == "shell" {
+
+    onShell(w, r)
+    return
+  }
+  // serve static assets from 'static' dir:
+  h.fileServer.ServeHTTP(w, r)
 }
 
 // GET /shell handler
 // Launches /bin/bash and starts serving it via the terminal
 func onShell(w http.ResponseWriter, r *http.Request) {
-	wsHandler := func(ws *websocket.Conn) {
-		// wrap the websocket into UTF-8 wrappers:
-		wrapper := NewWebSockWrapper(ws, WebSocketTextMode)
-		stdout := wrapper
-		stderr := wrapper
+  wsHandler := func(ws *websocket.Conn) {
+    // wrap the websocket into UTF-8 wrappers:
+    wrapper := NewWebSockWrapper(ws, WebSocketTextMode)
+    stdout := wrapper
+    stderr := wrapper
 
-		// this one is optional (solves some weird issues with vim running under shell)
-		stdin := &InputWrapper{ws}
+    // this one is optional (solves some weird issues with vim running under shell)
+    stdin := &InputWrapper{ws}
 
-		// starts new command in a newly allocated terminal:
-		// TODO: replace /bin/bash with:
-		//		 kubectl exec -ti <pod> --container <container name> -- /bin/bash
-		cmd := exec.Command("/bin/bash")
-		tty, err := pty.Start(cmd)
-		if err != nil {
-			panic(err)
-		}
-		defer tty.Close()
+    // starts new command in a newly allocated terminal:
+    // TODO: replace /bin/bash with:
+    // kubectl exec -ti <pod> --container <container name> -- /bin/bash
+    // pipe to/fro websocket to the TTY:
+    go func() {
+      io.Copy(stdout, tty)
+    }()
+    go func() {
+      io.Copy(stderr, tty)
+    }()
+    go func() {
+      io.Copy(tty, stdin)
+    }()
 
-		// pipe to/fro websocket to the TTY:
-		go func() {
-			io.Copy(stdout, tty)
-		}()
-		go func() {
-			io.Copy(stderr, tty)
-		}()
-		go func() {
-			io.Copy(tty, stdin)
-		}()
+    // wait for the command to exit, then close the websocket
+    //cmd.Wait()
 
-		// wait for the command to exit, then close the websocket
-		cmd.Wait()
-	}
-	defer log.Printf("Websocket session closed for %v", r.RemoteAddr)
+    liveCount = HEARTBEATCOUNT
+    for {
+      if debug == true { 
+        fmt.Printf("%s | live client. count: %d\n", r.RemoteAddr,liveCount)
+      }
+      if liveCount > 0 { liveCount = liveCount - 1 }
+      if liveCount < 1 {
+        ws.Close()
+        if debug == true {
+          fmt.Printf("%s | timeout.\n", r.RemoteAddr)
+        }
+        return
+      }
+      time.Sleep(time.Duration(HEARTBEAT) * time.Second)
+    }
+  }
+  defer log.Printf("Websocket session closed for %v", r.RemoteAddr)
 
-	// start the websocket session:
-	websocket.Handler(wsHandler).ServeHTTP(w, r)
+  // start the websocket session:
+  websocket.Handler(wsHandler).ServeHTTP(w, r)
+}
+
+func Exists(name string) bool {
+    _, err := os.Stat(name)
+    return !os.IsNotExist(err)
+}
+
+
+func (this *InputWrapper) Read(out []byte) (n int, err error) {
+  var data []byte
+  err = websocket.Message.Receive(this.ws, &data)
+  if err != nil {
+    return 0, io.EOF
+  }
+
+  liveCount = HEARTBEATCOUNT
+
+  if len(data) >= patternLen {
+    for i := range ignoredInputs {
+      pattern := ignoredInputs[i]
+      if bytes.Equal(pattern[:], data[:patternLen]) {
+        return 0, nil
+      }
+    }
+  }
+  return copy(out, data), nil
+}
+
+func execmd(command string) string {
+  out, err := exec.Command(os.Getenv("SHELL"), "-c", command + " 2>&1 | tee").Output()
+  if err != nil {
+    fmt.Println(err)
+    return ""
+  }
+  fmt.Printf("local exec command: %s : %s\n", command, out)
+  return strings.Replace(string(out),"\n","",-1)
 }
